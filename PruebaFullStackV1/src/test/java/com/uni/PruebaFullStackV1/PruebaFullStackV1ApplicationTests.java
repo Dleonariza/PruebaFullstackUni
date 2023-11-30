fc8590571ec2c113adfd5810379dc0cbe37dd180
Dleonariza <76213649+Dleonariza@users.noreply.github.com>
2023-11-29 22:49:12 -0500

    Main

---
+++ PruebaFullStackV1/src/test/java/com/uni/PruebaFullStackV1/PruebaFullStackV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.PruebaFullStackV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaFullStackV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
